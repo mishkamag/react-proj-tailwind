79d9337b195ac49a54a047aca1fd1681b6993991
mishkamag <m93.maglaperidze@gmail.com>
2022-12-03 22:03:11 +0400

    finished just css UI structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import About from "./components/About";
+import AllInOne from "./components/AllInOne";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Support from "./components/Support";
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
+      <Footer />
     </>
   );
 }
