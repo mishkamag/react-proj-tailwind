597a7f4b8d19b1a939ad0ec0a505653c4cd3188f
mishkamag <m93.maglaperidze@gmail.com>
2022-12-03 21:33:06 +0400

    added about page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import geocamp from "../assets/geocamp.jpg";
 
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
@@ -12,7 +13,7 @@ const Navbar = () => {
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
-          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">GEOCAMP.</h1>
+          <img className="w-12 h-auto" src={geocamp} alt="/" />
           <ul className="hidden md:flex">
             <li>Home</li>
             <li>About</li>
